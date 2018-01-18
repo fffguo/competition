ac83c15095fd9da73d860d4897df459870fd2391
胡鹏鹏 <1171132590@qq.com>
2018-01-18 12:17:10 +0800

    aaaaaaaaaaaaaa

--- src/main/java/com/example/competition/Generator.java
+++ src/main/java/com/example/competition/Generator.java
@@ -23,7 +23,7 @@ class Generator {
         DefaultShellCallback callback = new DefaultShellCallback(overwrite);
         MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
         myBatisGenerator.generate(null);
-
+/*aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa*/
         System.out.println();
     }
 }
