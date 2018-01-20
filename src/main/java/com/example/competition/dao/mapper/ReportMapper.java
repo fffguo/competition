44362442ce84082475aa794db3efb321b2ba481b
刘富国 <595726017@qq.com>
2018-01-20 16:57:37 +0800

    逆向工程更新

---
+++ src/main/java/com/example/competition/dao/mapper/ReportMapper.java
@@ -0,0 +1,30 @@
+package com.example.competition.dao.mapper;
+
+import com.example.competition.dao.entity.Report;
+import com.example.competition.dao.entity.ReportExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface ReportMapper {
+    long countByExample(ReportExample example);
+
+    int deleteByExample(ReportExample example);
+
+    int deleteByPrimaryKey(Integer reportId);
+
+    int insert(Report record);
+
+    int insertSelective(Report record);
+
+    List<Report> selectByExample(ReportExample example);
+
+    Report selectByPrimaryKey(Integer reportId);
+
+    int updateByExampleSelective(@Param("record") Report record, @Param("example") ReportExample example);
+
+    int updateByExample(@Param("record") Report record, @Param("example") ReportExample example);
+
+    int updateByPrimaryKeySelective(Report record);
+
+    int updateByPrimaryKey(Report record);
+}
